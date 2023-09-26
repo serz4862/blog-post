c474138fc98b587ab58adffe03ce8cb530bb47ba
serz4862 <sauravkumar4862@gmail.com>
2023-09-27 02:45:32 +0530

    blog-post design

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div> Test</div>
+    <main>
+      <header>
+        <a href="" className= "logo">Blog-post</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className= "entry">
+        <img src="client/public/F5GKnHVagAAn_gk.jpeg" alt=" myCar" />
+      </div>
+    </main>
   );
 
   
