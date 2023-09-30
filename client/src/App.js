978da4cc21c6324dcbce472b1ce1b9e53aef7d52
serz4862 <sauravkumar4862@gmail.com>
2023-09-30 13:09:42 +0530

    initiate

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Post from "./post";
 
 function App() {
   return (
@@ -11,21 +12,8 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
-      <div className= "Post">
-        <div className=" image">
-          <img src="https://imgd.aeplcdn.com/0x0/n/cw/ec/39472/a6-exterior-right-front-three-quarter.jpeg?q=80" alt=" myCar" />
-        </div>
-        <h2> The upcoming car</h2>
-        <p className="info"></p>
-          <a  className="author">Saurav kumar </a>
-          <time> 30-09-2023</time>
-        
-        <p>The Audi A6 is a series of executive cars manufactured by the German automaker Audi. It is positioned in the mid-size luxury sedan segment and has been in production since the mid-1990s. The A6 is known for its combination of performance, luxury, and advanced technology. Here are some key features and aspects associated with the Audi A6:
+      <Post/>
 
-</p>
-
-      </div>
-      <div className="content"></div>
     </main>
   );
 
